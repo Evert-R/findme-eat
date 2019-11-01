721986b6ed5db549344a3885f16219506b3bfc06
Evert Rot <evert.rot@gmx.com>
2019-11-01 15:06:14 +0100

    switched from fetch to serverside api, basic list and map

--- assets/js/api-access.js
+++ assets/js/api-access.js
@@ -19,7 +19,6 @@ function showResults(restaurants) {
 
     return `
         <div class="er-item-list">
-        <button id="sorting">Sort</button>
             <table>            
                 ${listItems.join("\n")}      
             </table>
@@ -27,49 +26,65 @@ function showResults(restaurants) {
     `
 }
 
-function getInput() { // get the search argument from the input field
-    let searchInput = document.getElementById("er-search-input").value;
+var map;
 
-    if (!searchInput) { // check if nothing was entered
-        $("#er-search-input").attr("placeholder", `You have to give me something!`);
-        return;
+function initMap() {
+    // Get input from input field
+  let searchInput = document.getElementById("er-search-input").value;
+    // Create the map.
+  map = new google.maps.Map(document.getElementById('map'), {
+
+    zoom: 17
+  });
+
+  // Create the places service.
+  var service = new google.maps.places.PlacesService(map);
+  var getNextPage = null;
+  var moreButton = document.getElementById('more');
+  moreButton.onclick = function() {
+    moreButton.disabled = true;
+    if (getNextPage) getNextPage();
+  };
+
+  // Perform a nearby search.
+
+  service.textSearch(
+      {query: searchInput+' vegetarian', type: ['restaurant']},
+      function(results, status, pagination) {
+        if (status !== 'OK') return;
+        $("#er-search-results").html(showResults(results));
+        console.log(results);
+        createMarkers(results);
+        moreButton.disabled = !pagination.hasNextPage;
+        getNextPage = pagination.hasNextPage && function() {
+          pagination.nextPage();
+        };
+      });
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
     };
-    // make search argument safe for url use
-    let uri = encodeURI(searchInput);
-    // create the uri to query the
-    let apiLink = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurant+vegetarian+${uri}&key=AIzaSyBvX8IlqhZmWM6jK2aL0sDwuKgYPIHqpKE`;
-    getData(apiLink);
+
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+
+
+
+    bounds.extend(place.geometry.location);
+  }
+  map.fitBounds(bounds);
 }
-
-function getData(uri) {
-    // empty div
-    $("#er-search-results").html("");
-    $.when(
-    //         $.get(uri)
-        $.get('assets/data/newYork.json') // test without api access
-    ).then(
-        // make variable of data and push to div
-        function (response) {
-            let searchResults = response;
-
-            $("#er-search-results").html(showResults(searchResults.results));
-            console.log(searchResults);
-        },
-        // error handling
-        function (errorResponse) {
-            if (errorResponse.status === 404) {
-                $("#er-api-data").html(
-                    `<h2>No info found for leiden</h2>`);
-            } else if (errorResponse.status === 403) {
-                let resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
-                $("#er-api-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
-            } else {
-                console.log(errorResponse);
-                $("#er-api-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-            }
-        });
-}
-
-
-
